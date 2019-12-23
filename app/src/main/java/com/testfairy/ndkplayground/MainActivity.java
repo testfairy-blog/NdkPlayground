21ddbd5e570f9193764e038f881b3df77a06ca6c
Diego Perini <diego@dperini.com>
2019-12-23 13:48:19 +0300

    Added TestFairy SDK and gradle plugin

--- app/src/main/java/com/testfairy/ndkplayground/MainActivity.java
+++ app/src/main/java/com/testfairy/ndkplayground/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.testfairy.TestFairy;
+
 public class MainActivity extends Activity {
 
 	// Used to load the 'native-lib' library on application startup.
@@ -17,44 +19,23 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		// TODO : set this before running the app
+		TestFairy.begin(this, "");
+
 		// Example of a call to a native method
 		Button crashButton = findViewById(R.id.crash_button);
 		crashButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				crashAndGetExceptionMessage();
+				crashNatively();
 			}
 		});
 	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-
-		initSignalHandler();
-	}
-
-	@Override
-	protected void onPause() {
-		super.onPause();
-
-		deinitSignalHandler();
-	}
-
-	/**
-	 * Initialize native signal handler to catch native crashes.
-	 */
-	public native void initSignalHandler();
-
-	/**
-	 * Deinitialzie native signal handler to leave native crashes alone.
-	 */
-	public native void deinitSignalHandler();
 
 	/**
 	 * A native method that is implemented by the 'native-lib' native library,
 	 * which is packaged with this application. It will throw a C++ exception
 	 * and catch it in the signal handler which will be visible in the logs.
 	 */
-	public native void crashAndGetExceptionMessage();
+	public native void crashNatively();
 }
